3e7ec2686c5baf362c5a1bd215228e302314f5eb
Adesida Leye <adeleyeadesida@gmail.com>
2022-11-10 15:39:49 +0100

    update

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let char = document.getElementById('char');
 let obs = document.getElementById('obs');
 
-document.addEventListener('keyup', e =>{
+document.addEventListener('keydown', e =>{
     if(e.key === '' || e.code === 'Space'){
         char.classList.toggle('animate')
+        }
+}) 
+
+const checkGame = setInterval(() =>{
+    const charCheck = parseInt(window.getComputedStyle(char).getPropertyValue('top'));
+    const obsCheck = parseInt(window.getComputedStyle(obs).getPropertyValue('left'));
+
+    if(obsCheck > 5 && obsCheck < 15 && charCheck >= 16){
+        obs.style.animation = 'none';
+        alert('GAME OVER LOSER!!')
     }
-})
+}, 10)
