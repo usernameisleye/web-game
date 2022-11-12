22fa295932abce4a636ad0220c34633144455ccc
Adesida Leye <adeleyeadesida@gmail.com>
2022-11-12 18:57:19 +0100

    update on main files

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let char = document.getElementById('char');
 let obs = document.getElementById('obs');
+let obs1 = document.getElementById('obs1');
 
 document.addEventListener('keydown', e =>{
     if(e.key === '' || e.code === 'Space'){
@@ -9,10 +10,11 @@ document.addEventListener('keydown', e =>{
 
 const checkGame = setInterval(() =>{
     const charCheck = parseInt(window.getComputedStyle(char).getPropertyValue('top'));
-    const obsCheck = parseInt(window.getComputedStyle(obs).getPropertyValue('left'));
+    const obsCheck = parseInt(window.getComputedStyle(obs && obs1).getPropertyValue('left'));
 
-    if(obsCheck > 5 && obsCheck < 15 && charCheck >= 16){
-        obs.style.animation = 'none';
-        alert('GAME OVER LOSER!!')
-    }
-}, 10)
+//     if(obsCheck > 6 && obsCheck < 8 && charCheck >= 8){
+//         obs.style.animation = 'none';
+//         obs.style.display = 'none';
+//         alert('GAME OVER LOSER!!')
+//     }
+// }, 10)
